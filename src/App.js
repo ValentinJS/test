c67b78b9483b5d0448e1ad833ea52ee6828abefb
Valentin Fournier <valentin.fournier-ext@ekino.com>
2020-09-30 18:27:42 +0200

    Add test2

--- src/App.js
+++ src/App.js
@@ -16,35 +16,69 @@ const Row = ({ index, style, data }) => {
   );
 };
 
-const Example = ({ isActive, selectedId }) => {
-  let ref;
+const Example = ({ isActive, selectedId, isActive2, selectedId2 }) => {
+  let ref, ref2;
   useEffect(() => {
     ref.scrollToItem(selectedId);
+    ref2.scrollToItem(selectedId2);
     //activeBinder("ListBinder", selectedId);
   });
 
   return (
-    <Binder id="ListBinder" selector="button" direction="horizontal" active>
-      <List
-        className="List"
-        height={200}
-        itemCount={1000}
-        itemSize={100}
-        itemData={{ selectedId, isActive }}
-        width={800}
-        // overscanCount={10}
-        layout="horizontal"
-        ref={(x) => {
-          ref = x;
-        }}
+    <React.Fragment>
+      <Binder
+        id="ListBinder"
+        selector="button"
+        direction="horizontal"
+        active
+        onDownExit="ListBinder2"
       >
-        {Row}
-      </List>
-    </Binder>
+        <List
+          className="List"
+          height={200}
+          itemCount={1000}
+          itemSize={100}
+          itemData={{ selectedId, isActive }}
+          width={800}
+          // overscanCount={10}
+          layout="horizontal"
+          ref={(x) => {
+            ref = x;
+          }}
+        >
+          {Row}
+        </List>
+      </Binder>
+      <Binder
+        id="ListBinder2"
+        selector="button"
+        direction="horizontal"
+        active
+        onUpExit="ListBinder"
+      >
+        <List
+          className="List List2"
+          height={200}
+          itemCount={1000}
+          itemSize={100}
+          itemData={{ selectedId: selectedId2, isActive: isActive2 }}
+          width={800}
+          // overscanCount={10}
+          layout="horizontal"
+          ref={(x) => {
+            ref2 = x;
+          }}
+        >
+          {Row}
+        </List>
+      </Binder>
+    </React.Fragment>
   );
 };
 
 export default connect(() => ({
   isActive: isBinderActive("ListBinder")(),
+  isActive2: isBinderActive("ListBinder2")(),
   selectedId: getBinderSelectedId("ListBinder")(),
+  selectedId2: getBinderSelectedId("ListBinder2")(),
 }))(Example);
