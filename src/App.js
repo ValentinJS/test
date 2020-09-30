7384dd7aec0987c41e375ae876c98494f356b3bc
Valentin Fournier <valentin.fournier-ext@ekino.com>
2020-09-30 18:12:07 +0200

    Test

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import { FixedSizeList as List } from "react-window";
+import { Binder, isBinderActive, getBinderSelectedId } from "react-keys";
+import { connect } from "react-redux";
 
-function App() {
+import "./App.css";
+
+const Row = ({ index, style, data }) => {
+  let className = index % 2 ? "ListItemOdd" : "ListItemEven";
+  className += data.selectedId === index.toString() ? " selected" : "";
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <button className={className} style={style} id={index}>
+      Index {index}
+    </button>
   );
-}
+};
 
-export default App;
+const Example = ({ isActive, selectedId }) => {
+  let ref;
+  useEffect(() => {
+    ref.scrollToItem(selectedId);
+    //activeBinder("ListBinder", selectedId);
+  });
+
+  return (
+    <Binder id="ListBinder" selector="button" direction="horizontal" active>
+      <List
+        className="List"
+        height={200}
+        itemCount={1000}
+        itemSize={100}
+        itemData={{ selectedId, isActive }}
+        width={800}
+        // overscanCount={10}
+        layout="horizontal"
+        ref={(x) => {
+          ref = x;
+        }}
+      >
+        {Row}
+      </List>
+    </Binder>
+  );
+};
+
+export default connect(() => ({
+  isActive: isBinderActive("ListBinder")(),
+  selectedId: getBinderSelectedId("ListBinder")(),
+}))(Example);
