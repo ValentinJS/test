56d988fcadf2e7e02c11443aef51aceec0f4dd51
Valentin Fournier <valentin.fournier-ext@ekino.com>
2020-10-02 15:31:29 +0200

    Repro bug : Autosize + float width

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import { FixedSizeList as List } from "react-window";
 import { Binder, isBinderActive, getBinderSelectedId } from "react-keys";
 import { connect } from "react-redux";
+import AutoSizer from "react-virtualized-auto-sizer";
 
 import "./App.css";
 
@@ -19,36 +20,54 @@ const Row = ({ index, style, data }) => {
 const Example = ({ isActive, selectedId, isActive2, selectedId2 }) => {
   let ref, ref2;
   useEffect(() => {
-    ref.scrollToItem(selectedId);
-    ref2.scrollToItem(selectedId2);
-    //activeBinder("ListBinder", selectedId);
+    ref && ref.scrollToItem(selectedId);
+    ref2 && ref2.scrollToItem(selectedId2);
   });
+
+  const onScroll = ({
+    scrollDirection,
+    scrollOffset,
+    scrollUpdateWasRequested,
+  }) => {
+    console.log({
+      scrollDirection,
+      scrollOffset,
+      scrollUpdateWasRequested,
+    });
+  };
 
   return (
     <React.Fragment>
-      <Binder
-        id="ListBinder"
-        selector="button"
-        direction="horizontal"
-        active
-        onDownExit="ListBinder2"
-      >
-        <List
-          className="List"
-          height={200}
-          itemCount={1000}
-          itemSize={100}
-          itemData={{ selectedId, isActive }}
-          width={800}
-          // overscanCount={10}
-          layout="horizontal"
-          ref={(x) => {
-            ref = x;
-          }}
-        >
-          {Row}
-        </List>
-      </Binder>
+      <div style={{ width: "100%", height: 200 }}>
+        <AutoSizer>
+          {({ width, height }) => (
+            <Binder
+              id="ListBinder"
+              selector="button"
+              direction="horizontal"
+              active
+              onDownExit="ListBinder2"
+            >
+              <List
+                className="List"
+                height={height}
+                itemCount={1000}
+                itemSize={100.4}
+                itemData={{ selectedId, isActive }}
+                width={width}
+                // overscanCount={10}
+                layout="horizontal"
+                ref={(x) => {
+                  ref = x;
+                }}
+                onScroll={onScroll}
+              >
+                {Row}
+              </List>
+            </Binder>
+          )}
+        </AutoSizer>
+      </div>
       <Binder
         id="ListBinder2"
         selector="button"
